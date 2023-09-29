74b5f0b1549ebf735616d9d961d6aa72d7903c99
Scott Taylor <scott.c.taylor@mac.com>
2023-09-29 01:02:02 -0400

    remix 2.0

--- remix.config.js
+++ remix.config.js
@@ -12,15 +12,4 @@ module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['**/.*'],
   publicPath,
-  serverDependenciesToBundle: [],
-  serverModuleFormat: 'cjs',
-  postcss: true,
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
 };
