22361095021085c969f638aac9b97c3b51af2fa3
Scott Taylor <scott.c.taylor@mac.com>
2023-11-03 14:28:34 -0400

    Update Toolbar.tsx

--- src/components/Editor/Toolbar.tsx
+++ src/components/Editor/Toolbar.tsx
@@ -7,7 +7,7 @@ const Toolbar = forwardRef<LegacyRef<HTMLDivElement>, any>(({ className, ...prop
     {...props}
     ref={ref}
     className={cn(
-      'absolute z-10 scale-0 rounded bg-white shadow-lg transition-transform',
+      'absolute z-50 scale-0 rounded bg-white shadow-lg transition-transform',
       'after:border-l-1.5 after:border-transparent after:border-t-white',
       'after:pointer-events-none after:absolute after:right-1/2 after:-bottom-3 after:h-0 after:w-0',
       'after:[&.Toolbar-flush]:border-t-transparent',
