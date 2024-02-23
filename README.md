a48cf3bf4c2ff80982f928a9b1254869d4d41579
Scott Taylor <scott.c.taylor@mac.com>
2024-02-23 15:56:16 -0500

    generate schema

---
+++ README.md
@@ -0,0 +1,1 @@
+# highforthis-remix
---
+++ apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'highforthis',
+      url: 'http://localhost:8080/graphql',
+      skipSSLValidation: true,
+    },
+  },
+};
